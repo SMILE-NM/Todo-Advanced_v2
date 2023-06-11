65ad66356324693652c9d41c9b3a65d9f8a83cca
Nematov Muhammadjon <123355875+SMILE-NM@users.noreply.github.com>
2023-06-11 21:53:59 +0500

    first commit

---
+++ src/components/TaskItem.jsx
@@ -0,0 +1,37 @@
+import { useState } from "react";
+//styles
+import styles from "./TaskItem.module.css";
+
+//Library
+import { CheckIcon } from "@heroicons/react/24/outline";
+
+const TaskItem = ({ task }) => {
+  const [isChecked, setIsChecked] = useState(task.checked);
+
+  const handleCheckboxChange = (e) => {
+    setIsChecked(!isChecked);
+  };
+
+  return (
+    <li className={styles.task}>
+      <div className={styles["task-group"]}>
+        <input
+          type="checkbox"
+          className={styles.checkbox}
+          checked={isChecked}
+          onChange={handleCheckboxChange}
+          name={task.name}
+          id={task.id}
+        />
+        <label htmlFor={task.id} className={styles.label}>
+          {task.name}
+          <p className={styles.checkmark}>
+            <CheckIcon strokeWidth={2} width={24} height={24} />
+          </p>
+        </label>
+      </div>
+    </li>
+  );
+};
+
+export default TaskItem;
